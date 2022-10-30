a705236a9796ed03d5a516c569a3785282857424
MRPhillip007 <filipplyamov@gmail.com>
2022-10-30 19:48:00 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+
+namespace AsyncHomework
+{
+    internal class Program
+    {
+        static string cityName = "Odessa";
+        static readonly string URL = $"http://api.openweathermap.org/data/2.5/weather?q={cityName}&units=metric&appid=642486029ae6aec1a5b402c0efdc47d7";
+
+        static async Task Main(string[] args)
+        {
+
+            //HttpWebRequest request = (HttpWebRequest) WebRequest.Create(URL);
+            //HttpWebResponse response = (HttpWebResponse) request.GetResponse();
+
+            //using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+            //{
+            //    string data = reader.ReadToEnd();
+            //    WeatherResponse ?weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(data);
+            //}
+            Console.Write("Enter city name: ");
+            cityName = Console.ReadLine()!;
+            
+
+            await GetTempByCityAsync(URL, cityName);
+
+        }
+
+        static async Task GetTempByCityAsync(string url, string city)
+        {
+            HttpClient client = new HttpClient();
+            string response = await client.GetStringAsync(url);
+
+            WeatherResponse ?weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(response);
+            Console.WriteLine($"In {city} - {weatherResponse!.Main.Temp} now");
+        }
+    }
+}
